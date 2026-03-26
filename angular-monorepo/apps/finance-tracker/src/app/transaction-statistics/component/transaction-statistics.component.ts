a5180622440027472a065cbbf6cff377b2ed2d6f
dashk <burckina.anna@yandex.ru>
2026-03-26 19:10:47 +0300

    Рефакторинг transaction-statistics.component.ts

--- angular-monorepo/apps/finance-tracker/src/app/transaction-statistics/component/transaction-statistics.component.ts
+++ angular-monorepo/apps/finance-tracker/src/app/transaction-statistics/component/transaction-statistics.component.ts
@@ -7,15 +7,17 @@ import {
 } from '@angular/core';
 import { TransactionsStorageService } from '../../transaction-form/services/transactions-storage.service';
 import { TuiRingChart } from '@taiga-ui/addon-charts';
-import { Transaction } from '../../transaction-form/types/transaction.types';
 import { tuiSum } from '@taiga-ui/cdk';
 import { AsyncPipe } from '@angular/common';
 import { TuiAmountPipe } from '@taiga-ui/addon-commerce';
-
-interface ChartItem {
-  readonly label: string;
-  readonly value: number;
-}
+import {
+  buildChartItems,
+  getActiveChartLabel,
+  getActiveChartSum,
+  mapChartLabels,
+  mapChartValues,
+} from '../utils/transaction-statistics.util';
+import { TransactionType } from '../../transaction-form/types/transaction-form.types';
 
 @Component({
   selector: 'app-transaction-statistics',
@@ -32,81 +34,55 @@ export class TransactionStatisticsComponent {
   readonly incomeIndex = signal<number>(NaN);
   readonly expenseIndex = signal<number>(NaN);
 
-  readonly incomeItems = computed(() =>
-    this.buildChartItems(
-      this.transactions().filter(
-        (transaction) => transaction.type === 'income',
-      ),
-    ),
-  );
+  readonly incomeItems = computed(() => this.getChartItemsByType('income'));
+  readonly expenseItems = computed(() => this.getChartItemsByType('expense'));
 
-  readonly expenseItems = computed(() =>
-    this.buildChartItems(
-      this.transactions().filter(
-        (transaction) => transaction.type === 'expense',
-      ),
-    ),
-  );
+  readonly incomeValues = computed(() => mapChartValues(this.incomeItems()));
+  readonly expenseValues = computed(() => mapChartValues(this.expenseItems()));
 
-  readonly incomeValues = computed(() =>
-    this.incomeItems().map((item) => item.value),
-  );
-
-  readonly expenseValues = computed(() =>
-    this.expenseItems().map((item) => item.value),
-  );
-
-  readonly incomeLabels = computed(() =>
-    this.incomeItems().map((item) => item.label),
-  );
-
-  readonly expenseLabels = computed(() =>
-    this.expenseItems().map((item) => item.label),
-  );
+  readonly incomeLabels = computed(() => mapChartLabels(this.incomeItems()));
+  readonly expenseLabels = computed(() => mapChartLabels(this.expenseItems()));
 
   readonly incomeTotal = computed(() => tuiSum(...this.incomeValues()));
   readonly expenseTotal = computed(() => tuiSum(...this.expenseValues()));
 
-  readonly incomeSum = computed(() => {
-    const index = this.incomeIndex();
-    const values = this.incomeValues();
+  readonly incomeSum = computed(() =>
+    getActiveChartSum(
+      this.incomeIndex(),
+      this.incomeValues(),
+      this.incomeTotal(),
+    ),
+  );
 
-    return (Number.isNaN(index) ? this.incomeTotal() : values[index]) ?? 0;
-  });
+  readonly expenseSum = computed(() =>
+    getActiveChartSum(
+      this.expenseIndex(),
+      this.expenseValues(),
+      this.expenseTotal(),
+    ),
+  );
 
-  readonly expenseSum = computed(() => {
-    const index = this.expenseIndex();
-    const values = this.expenseValues();
+  readonly incomeLabel = computed(() =>
+    getActiveChartLabel(
+      this.incomeIndex(),
+      this.incomeLabels(),
+      'Всего доходов',
+    ),
+  );
 
-    return (Number.isNaN(index) ? this.expenseTotal() : values[index]) ?? 0;
-  });
+  readonly expenseLabel = computed(() =>
+    getActiveChartLabel(
+      this.expenseIndex(),
+      this.expenseLabels(),
+      'Всего расходов',
+    ),
+  );
 
-  readonly incomeLabel = computed(() => {
-    const index = this.incomeIndex();
-    const labels = this.incomeLabels();
+  private getChartItemsByType(type: TransactionType) {
+    const filteredTransactions = this.transactions().filter(
+      (transaction) => transaction.type === type,
+    );
 
-    return (Number.isNaN(index) ? 'Всего доходов' : labels[index]) ?? '';
-  });
-
-  readonly expenseLabel = computed(() => {
-    const index = this.expenseIndex();
-    const labels = this.expenseLabels();
-
-    return (Number.isNaN(index) ? 'Всего расходов' : labels[index]) ?? '';
-  });
-
-  private buildChartItems(transactions: readonly Transaction[]): ChartItem[] {
-    const grouped = new Map<string, number>();
-
-    for (const transaction of transactions) {
-      const currentValue = grouped.get(transaction.category) ?? 0;
-
-      grouped.set(transaction.category, currentValue + transaction.amount);
-    }
-
-    return Array.from(grouped.entries()).map(([label, value]) => ({
-      label,
-      value,
-    }));
+    return buildChartItems(filteredTransactions);
   }
 }
